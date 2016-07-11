60ed6b72e1901936b3c498eee31779beb5e72e07
Mosen <mosen@users.noreply.github.com>
2016-07-11 22:07:22 +1000

    REMOVED: Flesh out implementation of acknowledging InstalledApplicationList response. Additional tags on Application for database. Applications datastore supports select with arbitrary where clauses Changed the signatures of all the ack handlers in connect service.
    (cherry picked from commit f16b776)

--- applications/application.go
+++ applications/application.go
@@ -1,14 +1,22 @@
 package applications
 
+import "database/sql"
+
 type Application struct {
-	Identifier   string `plist:",omitempty" json:"identifier,omitempty"`
-	Version      string `plist:",omitempty" json:"version,omitempty"`
-	ShortVersion string `plist:",omitempty" json:"short_version,omitempty"`
-	Name         string `json:"name,omitempty"`
-	BundleSize   int    `plist:",omitempty" json:"bundle_size,omitempty"`
+	UUID         string         `plist:",omitempty" json:"uuid,omitempty" db:"application_uuid"`
+	Identifier   sql.NullString `plist:",omitempty" json:"identifier,omitempty" db:"identifier"`
+	Version      sql.NullString `plist:",omitempty" json:"version,omitempty" db:"version"`
+	ShortVersion sql.NullString `plist:",omitempty" json:"short_version,omitempty" db:"short_version"`
+	Name         string         `json:"name,omitempty" db:"name"`
+	BundleSize   sql.NullInt64  `plist:",omitempty" json:"bundle_size,omitempty" db:"bundle_size"`
 
 	// The size of the app's document, library, and other folders, in bytes.
-	DynamicSize int `plist:",omitempty" json:"dynamic_size,omitempty"`
+	DynamicSize sql.NullInt64 `plist:",omitempty" json:"dynamic_size,omitempty" db:"dynamic_size"`
 
-	IsValidated bool `plist:",omitempty" json:"is_validated,omitempty"`
+	IsValidated sql.NullBool `plist:",omitempty" json:"is_validated,omitempty" db:"is_validated"`
+}
+
+type DeviceApplication struct {
+	DeviceUUID      string `json:"device_uuid" db:"device_uuid"`
+	ApplicationUUID string `json:"application_uuid" db:"application_uuid"`
 }
