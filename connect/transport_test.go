15f3e83a55e7f2dfa61d070fb9d935d948712176
Mosen <mosen@users.noreply.github.com>
2016-10-25 21:54:43 +1100

    Create skeleton test suite for connect transport

---
+++ connect/transport_test.go
@@ -0,0 +1,23 @@
+package connect
+
+import (
+	"github.com/jmoiron/sqlx"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func setup() {
+
+}
+
+func teardown() {
+
+}
+
+func TestDeviceInformationResponse(t *testing.T) {
+	setup()
+	defer teardown()
+
+}
