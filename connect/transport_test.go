f2248a8cee3434820a10fc836e50dd35f08f449e
Mosen <mosen@users.noreply.github.com>
2016-10-26 23:12:35 +1100

    Added mock command service. Add single test for InstalledApplicationList Add some modified real world responses as test data.

--- connect/transport_test.go
+++ connect/transport_test.go
@@ -1,18 +1,47 @@
 package connect
 
 import (
+	"bytes"
 	"database/sql"
 	"github.com/DavidHuie/gomigrate"
 	"github.com/go-kit/kit/log"
+	"github.com/micromdm/mdm"
 	"github.com/micromdm/micromdm/application"
 	"github.com/micromdm/micromdm/certificate"
 	"github.com/micromdm/micromdm/command"
 	"github.com/micromdm/micromdm/device"
 	"golang.org/x/net/context"
+	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 )
+
+type mockCommandService struct{}
+
+func (svc mockCommandService) NewCommand(*mdm.CommandRequest) (*mdm.Payload, error) {
+	return nil, nil
+}
+
+func (svc mockCommandService) NextCommand(udid string) ([]byte, int, error) {
+	return nil, 0, nil
+}
+
+func (svc mockCommandService) DeleteCommand(deviceUDID, commandUUID string) (int, error) {
+	return 0, nil
+}
+
+func (svc mockCommandService) Commands(deviceUDID string) ([]mdm.Payload, error) {
+	return []mdm.Payload{}, nil
+}
+
+func (svc mockCommandService) Find(commandUUID string) (*mdm.Payload, error) {
+	cmd := mdm.CommandRequest{
+		RequestType: "InstalledApplicationList",
+	}
+	return mdm.NewPayload(&cmd)
+}
 
 type connectFixtures struct {
 	db      *sql.DB
@@ -30,13 +59,12 @@ func setup(t *testing.T) *connectFixtures {
 	logger := log.NewContext(l).With("source", "testing")
 
 	var (
-		err           error
-		testConn      string = "user=postgres password= dbname=travis_ci_test sslmode=disable"
-		testRedisConn string = ":6379"
-		devices       device.Datastore
-		apps          application.Datastore
-		certs         certificate.Datastore
-		cs            command.Service
+		err      error
+		testConn string = "user=postgres password= dbname=travis_ci_test sslmode=disable"
+		devices  device.Datastore
+		apps     application.Datastore
+		certs    certificate.Datastore
+		cs       command.Service
 	)
 
 	db, err := sql.Open("postgres", testConn)
@@ -64,11 +92,7 @@ func setup(t *testing.T) *connectFixtures {
 		t.Fatal(err)
 	}
 
-	commandDB, err := command.NewDB("redis", testRedisConn, logger)
-	if err != nil {
-		t.Fatal(err)
-	}
-	cs = command.NewService(commandDB)
+	cs = mockCommandService{}
 
 	svc := NewService(devices, apps, certs, cs)
 	handler := ServiceHandler(ctx, svc, logger)
@@ -102,8 +126,34 @@ func teardown(fixtures *connectFixtures) {
 	fixtures.db.Exec(drop)
 }
 
-func TestDeviceInformationResponse(t *testing.T) {
+func TestInstalledApplicationListResponse(t *testing.T) {
 	fixtures := setup(t)
 	defer teardown(fixtures)
+
+	// create the faux command in the command service because connect will search for a match
+	cmd := mdm.CommandRequest{
+		UDID:        "00000000-1111-2222-3333-444455556666",
+		RequestType: "InstalledApplicationList",
+	}
+	fixtures.cs.NewCommand(&cmd)
+
+	requestBody, err := ioutil.ReadFile("../testdata/responses/macos/10.11.x/installed_application_list.plist")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client := http.DefaultClient
+	theURL := fixtures.server.URL + "/mdm/connect"
+	req, err := http.NewRequest("PUT", theURL, bytes.NewReader(requestBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(response.Status)
 
 }
