1ea0c6a941e8a58a0eabe3afe4a382e36aec6977
Jesse Peterson <jesse.c.peterson@gmail.com>
2017-03-19 18:15:57 -0700

    Keep conf nil for no DEP config nor no depsim

--- serve.go
+++ serve.go
@@ -479,7 +479,7 @@ func (c *config) depClient() (dep.Client, error) {
 	}
 	// depsim config
 	depsim := c.depsim
-	var conf *dep.Config
+	var conf, boltConf *dep.Config
 	if depsim {
 		conf = &dep.Config{
 			ConsumerKey:    "CK_48dd68d198350f51258e885ce9a5c37ab7f98543c4a697323d75682a6c10a32501cb247e3db08105db868f73f2c972bdb6ae77112aea803b9219eb52689d42e6",
@@ -488,7 +488,7 @@ func (c *config) depClient() (dep.Client, error) {
 			AccessSecret:   "AS_c31afd7a09691d83548489336e8ff1cb11b82b6bca13f793344496a556b1f4972eaff4dde6deb5ac9cf076fdfa97ec97699c34d515947b9cf9ed31c99dded6ba",
 		}
 	} else {
-		conf = &dep.Config{}
+		boltConf = new(dep.Config)
 	}
 
 	// try getting the oauth config from bolt
@@ -506,6 +506,9 @@ func (c *config) depClient() (dep.Client, error) {
 		err := json.Unmarshal(v, &token)
 		if err != nil {
 			return err
+		}
+		if !depsim {
+			conf = boltConf
 		}
 		conf.ConsumerSecret = token.ConsumerSecret
 		conf.ConsumerKey = token.ConsumerKey
