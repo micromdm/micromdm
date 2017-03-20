727d517d7f9647a7f7d339737fbde908bed605c9
Jesse Peterson <jesse.c.peterson@gmail.com>
2017-03-19 17:56:29 -0700

    Save an init

--- serve.go
+++ serve.go
@@ -479,7 +479,7 @@ func (c *config) depClient() (dep.Client, error) {
 	}
 	// depsim config
 	depsim := c.depsim
-	conf := &dep.Config{}
+	var conf *dep.Config
 	if depsim {
 		conf = &dep.Config{
 			ConsumerKey:    "CK_48dd68d198350f51258e885ce9a5c37ab7f98543c4a697323d75682a6c10a32501cb247e3db08105db868f73f2c972bdb6ae77112aea803b9219eb52689d42e6",
@@ -487,6 +487,8 @@ func (c *config) depClient() (dep.Client, error) {
 			AccessToken:    "AT_927696831c59ba510cfe4ec1a69e5267c19881257d4bca2906a99d0785b785a6f6fdeb09774954fdd5e2d0ad952e3af52c6d8d2f21c924ba0caf4a031c158b89",
 			AccessSecret:   "AS_c31afd7a09691d83548489336e8ff1cb11b82b6bca13f793344496a556b1f4972eaff4dde6deb5ac9cf076fdfa97ec97699c34d515947b9cf9ed31c99dded6ba",
 		}
+	} else {
+		conf = &dep.Config{}
 	}
 
 	// try getting the oauth config from bolt
