0fa2676337abfb3ce7dc997e9a360429d140dd0f
Brett Demetris <bdemetris@gmail.com>
2018-01-01 15:19:36 -0800

    roll back development code, cleanup

--- cmd/mdmctl/get.go
+++ cmd/mdmctl/get.go
@@ -100,7 +100,7 @@ Examples:
   # Get a list of devices
   mdmctl get devices
 
-  # Get a device by serial (TODO implement filtering)
+  # Get a device by serial
   mdmctl get devices -serial=C02ABCDEF
 `
 	fmt.Println(getUsage)
