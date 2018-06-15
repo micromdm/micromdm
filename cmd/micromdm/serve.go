2b7ff5fd51554c67a2f30c2e7843080c0ab1cfa3
Jesse Peterson <jcp@fb.com>
2018-06-15 14:30:32 -0700

    Use additional logging middleware

--- cmd/micromdm/serve.go
+++ cmd/micromdm/serve.go
@@ -222,10 +222,11 @@ func serve(args []string) error {
 	appDB := &appsbuiltin.Repo{Path: *flRepoPath}
 
 	scepEndpoints := scep.MakeServerEndpoints(sm.scepService)
-	// TODO: these were taken from cmd/scepserver/scepserver.go, do we need them?
-	// e.GetEndpoint = scepserver.EndpointLoggingMiddleware(lginfo)(e.GetEndpoint)
-	// e.PostEndpoint = scepserver.EndpointLoggingMiddleware(lginfo)(e.PostEndpoint)
-	scepHandler := scep.MakeHTTPHandler(scepEndpoints, sm.scepService, log.With(logger, "component", "scep"))
+	scepComponentLogger := log.With(logger, "component", "scep")
+	scepEndpoints.GetEndpoint = scep.EndpointLoggingMiddleware(scepComponentLogger)(scepEndpoints.GetEndpoint)
+	scepEndpoints.PostEndpoint = scep.EndpointLoggingMiddleware(scepComponentLogger)(scepEndpoints.PostEndpoint)
+	scepHandler := scep.MakeHTTPHandler(scepEndpoints, sm.scepService, scepComponentLogger)
+
 	enrollHandlers := enroll.MakeHTTPHandlers(ctx, enroll.MakeServerEndpoints(sm.enrollService, sm.scepDepot), httptransport.ServerErrorLogger(httpLogger))
 
 	r, options := httputil2.NewRouter(logger)
