bc75e40238f22abf22653f8993d74de8f37b437a
Scott Knight <4534275+knightsc@users.noreply.github.com>
2018-08-23 11:21:27 -0400

    Keep mdmctl remove device flags singular

--- cmd/mdmctl/remove_devices.go
+++ cmd/mdmctl/remove_devices.go
@@ -14,24 +14,24 @@ import (
 func (cmd *removeCommand) removeDevices(args []string) error {
 	flagset := flag.NewFlagSet("remove-devices", flag.ExitOnError)
 	var (
-		flIdentifiers = flagset.String("udids", "", "comma separated list of device UDIDs")
-		flSerials     = flagset.String("serials", "", "comma separated list of device serials")
+		flIdentifier = flagset.String("udid", "", "device UDID, optionally comma separated")
+		flSerial     = flagset.String("serial", "", "device serial, optionally comma separated")
 	)
 	flagset.Usage = usageFor(flagset, "mdmctl remove devices [flags]")
 	if err := flagset.Parse(args); err != nil {
 		return err
 	}
 
-	if *flIdentifiers == "" && *flSerials == "" {
+	if *flIdentifier == "" && *flSerial == "" {
 		return errors.New("bad input: device UDID or Serial must be provided")
 	}
 
 	opts := device.RemoveDevicesOptions{}
-	if *flIdentifiers != "" {
-		opts.UDIDs = strings.Split(*flIdentifiers, ",")
+	if *flIdentifier != "" {
+		opts.UDIDs = strings.Split(*flIdentifier, ",")
 	}
-	if *flSerials != "" {
-		opts.Serials = strings.Split(*flSerials, ",")
+	if *flSerial != "" {
+		opts.Serials = strings.Split(*flSerial, ",")
 	}
 
 	ctx := context.Background()
