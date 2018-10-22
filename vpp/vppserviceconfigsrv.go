76937e9e6f4fdb31cd26e7ce1b777d62f3036ee2
Jacob Jensen <jacob-jensen@bethel.edu>
2018-10-22 11:12:21 -0500

    Created Error Struct
    Removed the empty interface from the VPPServiceConfigSrv struct and replaced it with an array of Error structs.

--- vpp/vppserviceconfigsrv.go
+++ vpp/vppserviceconfigsrv.go
@@ -4,24 +4,29 @@ import "github.com/pkg/errors"
 
 // Contains the most recent data from Apple for configuring vpp
 type VPPServiceConfigSrv struct {
-	EditUserSrvURL                   string      `json:"editUserSrvUrl"`
-	DisassociateLicenseSrvURL        string      `json:"disassociateLicenseSrvUrl"`
-	ContentMetadataLookupURL         string      `json:"contentMetadataLookupUrl"`
-	ClientConfigSrvURL               string      `json:"clientConfigSrvUrl"`
-	GetUserSrvURL                    string      `json:"getUserSrvUrl"`
-	GetUsersSrvURL                   string      `json:"getUsersSrvUrl"`
-	GetLicensesSrvURL                string      `json:"getLicensesSrvUrl"`
-	GetVPPAssetsSrvURL               string      `json:"getVPPAssetsSrvUrl"`
-	VppWebsiteURL                    string      `json:"vppWebsiteUrl"`
-	InvitationEmailURL               string      `json:"invitationEmailUrl"`
-	RetireUserSrvURL                 string      `json:"retireUserSrvUrl"`
-	AssociateLicenseSrvURL           string      `json:"associateLicenseSrvUrl"`
-	ManageVPPLicensesByAdamIdSrvURL  string      `json:"manageVPPLicensesByAdamIdSrvUrl"`
-	RegisterUserSrvURL               string      `json:"registerUserSrvUrl"`
-	MaxBatchAssociateLicenseCount    int         `json:"maxBatchAssociateLicenseCount"`
-	MaxBatchDisassociateLicenseCount int         `json:"maxBatchDisassociateLicenseCount"`
-	Status                           int         `json:"status"`
-	ErrorCodes                       interface{} `json:"errorCodes"`
+	EditUserSrvURL                   string  `json:"editUserSrvUrl"`
+	DisassociateLicenseSrvURL        string  `json:"disassociateLicenseSrvUrl"`
+	ContentMetadataLookupURL         string  `json:"contentMetadataLookupUrl"`
+	ClientConfigSrvURL               string  `json:"clientConfigSrvUrl"`
+	GetUserSrvURL                    string  `json:"getUserSrvUrl"`
+	GetUsersSrvURL                   string  `json:"getUsersSrvUrl"`
+	GetLicensesSrvURL                string  `json:"getLicensesSrvUrl"`
+	GetVPPAssetsSrvURL               string  `json:"getVPPAssetsSrvUrl"`
+	VppWebsiteURL                    string  `json:"vppWebsiteUrl"`
+	InvitationEmailURL               string  `json:"invitationEmailUrl"`
+	RetireUserSrvURL                 string  `json:"retireUserSrvUrl"`
+	AssociateLicenseSrvURL           string  `json:"associateLicenseSrvUrl"`
+	ManageVPPLicensesByAdamIdSrvURL  string  `json:"manageVPPLicensesByAdamIdSrvUrl"`
+	RegisterUserSrvURL               string  `json:"registerUserSrvUrl"`
+	MaxBatchAssociateLicenseCount    int     `json:"maxBatchAssociateLicenseCount"`
+	MaxBatchDisassociateLicenseCount int     `json:"maxBatchDisassociateLicenseCount"`
+	Status                           int     `json:"status"`
+	ErrorCodes                       []Error `json:"errorCodes"`
+}
+
+type Error struct {
+	ErrorMessage string `json:"errorMessage"`
+	ErrorNumber  int    `json:"errorNumber"`
 }
 
 func (c *Client) GetVPPServiceConfigSrv() (*VPPServiceConfigSrv, error) {
