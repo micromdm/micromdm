4de9647773278d849f060e9f6742ee0f76920321
Emmanuel Goh <emmanuel@visenze.com>
2018-10-25 17:41:56 +0800

    Allow singulars for devices (also sort imports)

--- cmd/mdmctl/get.go
+++ cmd/mdmctl/get.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/x509"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -10,15 +11,12 @@ import (
 	"strings"
 	"text/tabwriter"
 
-	"crypto/x509"
-
 	"github.com/go-kit/kit/log"
-	"github.com/pkg/errors"
-
 	"github.com/micromdm/micromdm/pkg/crypto"
 	"github.com/micromdm/micromdm/platform/blueprint"
 	"github.com/micromdm/micromdm/platform/device"
 	"github.com/micromdm/micromdm/platform/profile"
+	"github.com/pkg/errors"
 )
 
 type getCommand struct {
@@ -55,6 +53,10 @@ func (cmd *getCommand) Run(args []string) error {
 	var run func([]string) error
 	switch strings.ToLower(args[0]) {
 	case "devices":
+		run = cmd.getDevices
+	case "dev":
+		run = cmd.getDevices
+	case "device":
 		run = cmd.getDevices
 	case "dep-devices":
 		run = cmd.getDEPDevices
