fecf81dc3db31e4ce28ea6384373b3324c1776c2
Tobias Baube <tobias.baube@abacus.ch>
2018-12-07 13:11:47 +0100

    Adds webhook files

---
+++ webhook/webhook.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, abort
+import base64
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	print(request.json)
+	if 'acknowledge_event' in request.json:
+		raw_payload = request.json['acknowledge_event']['raw_payload']
+		payload = base64.b64decode(raw_payload).decode('utf-8')
+		print(payload)
+	elif 'checkin_event' in request.json:
+		print("New Device was registered")
+		raw_payload = request.json['checkin_event']['raw_payload']
+		payload = base64.b64decode(raw_payload).decode('utf-8')
+		print(payload)
+	return ''
+
+if __name__ == '__main__':
+    app.run()
