c83aba4ccf3e28f5bc989abe715921a2a37ee0cd
Tobias Baube <tobias.baube@abacus.ch>
2018-12-18 14:23:32 +0100

    Server modified to allow missing DEP Certs and removed some logs

--- cmd/mdmctl/apply_dep_profile.go
+++ cmd/mdmctl/apply_dep_profile.go
@@ -24,7 +24,7 @@ func certificatesFromURL(serverURL string, insecure bool) ([]*x509.Certificate, 
 	}
 	addr := urlParsed.Host
 	if urlParsed.Port() == "" {
-		addr += ":443"
+		addr += ":8443"
 	}
 	conn, err := tls.Dial("tcp", addr, &tls.Config{InsecureSkipVerify: insecure})
 	if err != nil {
