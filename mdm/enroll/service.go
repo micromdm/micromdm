6a0b2136b044d465db47e83c96acb0dfd7c48543
Tobias Baube <tobias.baube@abacus.ch>
2019-01-02 13:54:23 +0100

    Profile Names changed for enrollment profile

--- mdm/enroll/service.go
+++ mdm/enroll/service.go
@@ -167,14 +167,14 @@ const perUserConnections = "com.apple.mdm.per-user-connections"
 func (svc *service) MakeEnrollmentProfile() (Profile, error) {
 	profile := NewProfile()
 	profile.PayloadIdentifier = EnrollmentProfileId
-	profile.PayloadOrganization = "MicroMDM"
+	profile.PayloadOrganization = "Abacus Research AG"
 	profile.PayloadDisplayName = "Enrollment Profile"
 	profile.PayloadDescription = "The server may alter your settings"
 	profile.PayloadScope = "System"
 
 	mdmPayload := NewPayload("com.apple.mdm")
 	mdmPayload.PayloadDescription = "Enrolls with the MDM server"
-	mdmPayload.PayloadOrganization = "MicroMDM"
+	mdmPayload.PayloadOrganization = "Abacus Research AG"
 	mdmPayload.PayloadIdentifier = EnrollmentProfileId + ".mdm"
 	mdmPayload.PayloadScope = "System"
 
@@ -213,7 +213,7 @@ func (svc *service) MakeEnrollmentProfile() (Profile, error) {
 		scepPayload.PayloadDescription = "Configures SCEP"
 		scepPayload.PayloadDisplayName = "SCEP"
 		scepPayload.PayloadIdentifier = EnrollmentProfileId + ".scep"
-		scepPayload.PayloadOrganization = "MicroMDM"
+		scepPayload.PayloadOrganization = "Abacus Research AG"
 		scepPayload.PayloadContent = scepContent
 		scepPayload.PayloadScope = "System"
 
@@ -226,8 +226,8 @@ func (svc *service) MakeEnrollmentProfile() (Profile, error) {
 	// Client needs to trust us at this point if we are using a self signed certificate.
 	if len(svc.TLSCert) > 0 {
 		tlsPayload := NewPayload("com.apple.security.pem")
-		tlsPayload.PayloadDisplayName = "Self-signed TLS certificate for MicroMDM"
-		tlsPayload.PayloadDescription = "Installs the TLS certificate for MicroMDM"
+		tlsPayload.PayloadDisplayName = "Self-signed TLS certificate for Abacus Research AG"
+		tlsPayload.PayloadDescription = "Installs the TLS certificate for Abacus Research AG"
 		tlsPayload.PayloadIdentifier = EnrollmentProfileId + ".cert.selfsigned"
 		tlsPayload.PayloadContent = svc.TLSCert
 
@@ -247,9 +247,9 @@ func (svc *service) OTAEnroll(ctx context.Context) (profile.Mobileconfig, error)
 func (svc *service) MakeOTAEnrollPayload() (Payload, error) {
 	payload := NewPayload("Profile Service")
 	payload.PayloadIdentifier = OTAProfileId
-	payload.PayloadDisplayName = "MicroMDM Profile Service"
+	payload.PayloadDisplayName = "Abacus Research Profile Service"
 	payload.PayloadDescription = "Profile Service enrollment"
-	payload.PayloadOrganization = "MicroMDM"
+	payload.PayloadOrganization = "Abacus Research"
 	payload.PayloadContent = ProfileServicePayload{
 		URL:              svc.URL + "/ota/phase23",
 		Challenge:        "",
@@ -268,7 +268,7 @@ func (svc *service) OTAPhase2(ctx context.Context) (profile.Mobileconfig, error)
 func (svc *service) MakeOTAPhase2Profile() (Profile, error) {
 	profile := NewProfile()
 	profile.PayloadIdentifier = OTAProfileId + ".phase2"
-	profile.PayloadOrganization = "MicroMDM"
+	profile.PayloadOrganization = "Abacus Research AG"
 	profile.PayloadDisplayName = "OTA Phase 2"
 	profile.PayloadDescription = "The server may alter your settings"
 	profile.PayloadScope = "System"
@@ -290,7 +290,7 @@ func (svc *service) MakeOTAPhase2Profile() (Profile, error) {
 	scepPayload.PayloadDescription = "Configures SCEP"
 	scepPayload.PayloadDisplayName = "SCEP"
 	scepPayload.PayloadIdentifier = OTAProfileId + ".phase2.scep"
-	scepPayload.PayloadOrganization = "MicroMDM"
+	scepPayload.PayloadOrganization = "Abacus Research AG"
 	scepPayload.PayloadContent = scepContent
 	scepPayload.PayloadScope = "System"
 
