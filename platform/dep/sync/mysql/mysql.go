c1f0af47df049883fbfca57f36da79ba62ce01bc
Tobias Baube <tobias.baube@abacus.ch>
2019-01-03 17:36:49 +0100

    Fixes issue with zero offset of time calculation resulting in zero timestamps

--- platform/dep/sync/mysql/mysql.go
+++ platform/dep/sync/mysql/mysql.go
@@ -76,6 +76,10 @@ func (d *Mysql) SaveCursor(ctx context.Context, cursor sync.Cursor) error {
 	// Make sure we take the time offset into account for "zero" dates	
 	t := time.Now()
 	_, offset := t.Zone()
+	// Don't multiply by zero
+	if (offset <= 0) {
+		offset = 1
+	}
 	var min_timestamp_sec int64 = int64(offset) * 60 * 60 * 24
 	
 	if (cursor.CreatedAt.IsZero() || cursor.CreatedAt.Unix() < min_timestamp_sec) {
