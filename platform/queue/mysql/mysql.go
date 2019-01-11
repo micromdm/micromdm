f83065b5937954d6ccc506ab53c1df7e34227229
Tobias Baube <tobias.baube@abacus.ch>
2019-01-11 14:15:45 +0100

    Database scheme adjusted for payload to be up to 16mb

--- platform/queue/mysql/mysql.go
+++ platform/queue/mysql/mysql.go
@@ -198,7 +198,7 @@ func SetupDB(db *sqlx.DB) error {
 	_,err = db.Exec(`CREATE TABLE IF NOT EXISTS `+DeviceCommandTable+` (
 	    uuid VARCHAR(40) PRIMARY KEY,
 	    device_udid VARCHAR(40) NOT NULL,
-	    payload BLOB DEFAULT NULL,
+	    payload MEDIUMBLOB DEFAULT NULL,
 	    created_at TIMESTAMP DEFAULT 0,
 	    last_sent_at TIMESTAMP DEFAULT 0,
 	    acknowledged_at TIMESTAMP DEFAULT 0,
@@ -210,6 +210,12 @@ func SetupDB(db *sqlx.DB) error {
 	if err != nil {
 	   return errors.Wrap(err, "creating "+DeviceCommandTable+" sql table failed")
 	}
+	
+	_,err = db.Exec(`ALTER TABLE `+DeviceCommandTable+` CHANGE payload MEDIUMBLOB DEFAULT NULL;`)
+	if err != nil {
+	   return errors.Wrap(err, "altering "+DeviceCommandTable+" sql table failed")
+	}
+
 	return nil
 }
 
