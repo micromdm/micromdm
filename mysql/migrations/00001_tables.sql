b0b1789dfbdeb3efbf6a3a729af5c203ce248f92
Tobias Baube <tobias.baube@abacus.ch>
2019-01-24 10:35:29 +0100

    Table creation added to migrations file

--- mysql/migrations/00001_tables.sql
+++ mysql/migrations/00001_tables.sql
@@ -3,8 +3,56 @@
 # Required for Timestamp defaults to "zero"
 SET sql_mode = '';
 
-CREATE TABLE IF NOT EXISTS devices (
+# platform/dep/sync/mysql
+CREATE TABLE IF NOT EXISTS cursors (
+    value VARCHAR(128) PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT 0
+);
+
+# platform/dep/sync/mysql
+CREATE TABLE IF NOT EXISTS dep_auto_assign (
+    profile_uuid VARCHAR(128) PRIMARY KEY,
+    filter TEXT DEFAULT NULL
+);
+
+# platform/profie/mysql
+CREATE TABLE IF NOT EXISTS profiles (
+	profile_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    identifier TEXT DEFAULT NULL,
+    mobileconfig BLOB DEFAULT NULL
+);
+
+# platform/scep/mysql
+CREATE TABLE IF NOT EXISTS server_config (
+	config_id INT PRIMARY KEY,
+    push_certificate BLOB DEFAULT NULL,
+    private_key BLOB DEFAULT NULL
+);
+
+# platform/scep/mysql
+CREATE TABLE IF NOT EXISTS scep_certificates (
+	scep_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	cert_name TEXT NULL,
+	scep_cert BLOB DEFAULT NULL
+) AUTO_INCREMENT=2;
+
+# platform/queue/mysql
+CREATE TABLE IF NOT EXISTS device_commands (
     uuid VARCHAR(40) PRIMARY KEY,
+    device_udid VARCHAR(40) NOT NULL,
+    payload BLOB DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT 0,
+    last_sent_at TIMESTAMP DEFAULT 0,
+    acknowledged_at TIMESTAMP DEFAULT 0,
+    times_sent int(11) DEFAULT 0,
+    last_status VARCHAR(32) DEFAULT NULL,
+    failure_message BLOB DEFAULT NULL,
+    command_order int(11) DEFAULT 0
+);
+
+# platform/devices/mysql
+CREATE TABLE IF NOT EXISTS devices (
+	uuid VARCHAR(40) PRIMARY KEY,
     udid VARCHAR(40) DEFAULT '',
     serial_number VARCHAR(12) DEFAULT '',
     os_version TEXT DEFAULT NULL,
@@ -29,16 +77,37 @@ CREATE TABLE IF NOT EXISTS devices (
     dep_profile_push_time TIMESTAMP DEFAULT 0,
     dep_profile_assigned_date TIMESTAMP DEFAULT 0,
     dep_profile_assigned_by TEXT DEFAULT NULL,
-	udid_cert_auth TEXT DEFAULT NULL,
     last_seen TIMESTAMP DEFAULT 0
 );
 
+# platform/devices/mysql
 CREATE TABLE IF NOT EXISTS uuid_cert_auth (
     udid VARCHAR(40) PRIMARY KEY,
     cert_auth BLOB DEFAULT NULL
 );
 
+# platform/remove/mysql
+CREATE TABLE IF NOT EXISTS remove_device (
+    udid VARCHAR(40) PRIMARY KEY
+);
 
+# platform/config/mysql
+CREATE TABLE IF NOT EXISTS server_config (
+	config_id INT PRIMARY KEY,
+    push_certificate BLOB DEFAULT NULL,
+    private_key BLOB DEFAULT NULL
+);
+
+# platform/config/mysql
+CREATE TABLE IF NOT EXISTS dep_tokens (
+	consumer_key VARCHAR(36) PRIMARY KEY,
+	consumer_secret TEXT NULL,
+	access_token TEXT NULL,
+	access_secret TEXT NULL,
+    access_token_expiry TIMESTAMP DEFAULT 0
+);
+
+# platform/apns/mysql
 CREATE TABLE IF NOT EXISTS push_info (
     udid VARCHAR(40) PRIMARY KEY,
     token TEXT DEFAULT '',
@@ -48,6 +117,15 @@ CREATE TABLE IF NOT EXISTS push_info (
 
 
 -- +goose Down
+DROP TABLE IF EXISTS cursors;
+DROP TABLE IF EXISTS dep_auto_assign;
+DROP TABLE IF EXISTS profiles;
+DROP TABLE IF EXISTS server_config;
+DROP TABLE IF EXISTS scep_certificates;
+DROP TABLE IF EXISTS device_commands;
 DROP TABLE IF EXISTS devices;
 DROP TABLE IF EXISTS uuid_cert_auth;
+DROP TABLE IF EXISTS remove_device;
+DROP TABLE IF EXISTS server_config;
+DROP TABLE IF EXISTS dep_tokens;
 DROP TABLE IF EXISTS push_info;
