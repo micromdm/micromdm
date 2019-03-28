89bd515bc950c21b6d86cbdf4f4faaa0335a3bb7
Tobias Baube <tobias.baube@abacus.ch>
2019-03-28 08:15:48 +0100

    scep-client-validity flag added to chart and docker script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,11 +11,13 @@ MYSQL_PASSWORD=${MYSQL_PASSWORD:-micromdm}
 MYSQL_DATABASE=${MYSQL_DATABASE:-micromdm}
 MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
 MYSQL_PORT=${MYSQL_PORT:-3306}
+SCEP_CLIENT_VALIDITY=${SCEP_CLIENT_VALIDITY:10000}
 
 micromdm serve \
   -config-path ${CONFIG_PATH} \
   -api-key ${API_KEY} \
   -server-url ${SERVER_URL} \
+  -scep-client-validity ${SCEP_CLIENT_VALIDITY} \
   -command-webhook-url ${WEBHOOK_URL} \
   -command-webhook-auth-user ${WEBHOOK_AUTH_USER} \
   -command-webhook-auth-pass ${WEBHOOK_AUTH_PASSWORD} \
