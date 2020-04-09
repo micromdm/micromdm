0dc190a2cda2dfa01a97064406a189292c5ebddf
Tobias Baube <tobias.baube@abacus.ch>
2020-04-09 17:49:28 +0200

    Fixes issue where initial setup with new device enrollment caused an error

--- platform/device/pg/pg.go
+++ platform/device/pg/pg.go
@@ -15,6 +15,49 @@ import (
 type Postgres struct{ db *sqlx.DB }
 
 func NewDB(db *sqlx.DB) (*Postgres, error) {
+	// Required for TIMESTAMP DEFAULT 0
+	_,err := db.Exec(`SET sql_mode = '';`)
+	
+	_,err = db.Exec(`CREATE TABLE IF NOT EXISTS `+tableName+` (
+		    uuid VARCHAR(40) PRIMARY KEY,
+		    udid VARCHAR(40) DEFAULT '',
+		    serial_number VARCHAR(12) DEFAULT '',
+		    os_version TEXT DEFAULT NULL,
+		    build_version TEXT DEFAULT NULL,
+		    product_name TEXT DEFAULT NULL,
+		    imei TEXT DEFAULT NULL,
+		    meid TEXT DEFAULT NULL,
+		    push_magic TEXT DEFAULT NULL,
+		    awaiting_configuration BOOLEAN DEFAULT false,
+		    token TEXT DEFAULT NULL,
+		    unlock_token TEXT DEFAULT NULL,
+		    enrolled BOOLEAN DEFAULT false,
+		    description TEXT DEFAULT NULL,
+		    model TEXT DEFAULT NULL,
+		    model_name TEXT DEFAULT NULL,
+		    device_name TEXT DEFAULT NULL,
+		    color TEXT DEFAULT NULL,
+		    asset_tag TEXT DEFAULT NULL,
+		    dep_profile_status TEXT DEFAULT NULL,
+		    dep_profile_uuid TEXT DEFAULT NULL,
+		    dep_profile_assign_time TIMESTAMPTZ DEFAULT '1970-01-01 00:00:00+00',
+		    dep_profile_push_time TIMESTAMPTZ DEFAULT '1970-01-01 00:00:00+00',
+		    dep_profile_assigned_date TIMESTAMPTZ DEFAULT '1970-01-01 00:00:00+00',
+		    dep_profile_assigned_by TEXT DEFAULT NULL,
+		    last_seen TIMESTAMPTZ DEFAULT '1970-01-01 00:00:00+00'
+		);`)
+	if err != nil {
+	   return nil, errors.Wrap(err, "creating devices sql table failed")
+	}
+	
+	_,err = db.Exec(`CREATE TABLE IF NOT EXISTS uuid_cert_auth (
+		    udid VARCHAR(40) PRIMARY KEY,
+		    cert_auth bytea DEFAULT NULL
+		);`)
+	if err != nil {
+	   return nil, errors.Wrap(err, "creating devices sql table failed")
+	}
+	
 	return &Postgres{db: db}, nil
 }
 
